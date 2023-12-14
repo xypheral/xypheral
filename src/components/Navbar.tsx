5c78bf1fee3a4ff8ebc4c5e44350f6127448bc2e
Wei Han <noelng26@gmail.com>
2023-12-14 15:34:34 +0800

    updates

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,6 +11,9 @@ const Navbar = () => {
                 <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-red-400 hover:text-red-300 transition duration-300">
                     <Link href="/about">About</Link>
                 </li>
+                <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-red-400 hover:text-red-300 transition duration-300">
+                    <Link href="/projects">Projects</Link>
+                </li>
                 <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-red-500 hover:text-red-300 transition duration-300">
                     <Link href="/contact">Contact</Link>
                 </li>
@@ -20,3 +23,37 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
+//                      NAVBAR DESIGN 1 
+// HOME - - - - - ABOUT - PROJECTS - CONTACT - - - - - - -
+
+// const Navbar = () => {
+//     return (
+//         <nav className="bg-gray-800 bg-opacity-10 p-4 mt-0 w-full fixed top-0 z-10">
+//             <div className="grid grid-cols-3">
+//                 <div>
+//                     <ul className="flex justify-start ml-5">
+//                         <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-red-400 hover:text-red-300 transition duration-300">
+//                             <Link href="/">Home</Link>
+//                         </li>
+//                     </ul>
+//                 </div>
+//                 <div>
+//                     <ul className="flex justify-center space-x-8">
+//                         <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-red-400 hover:text-red-300 transition duration-300">
+//                             <Link href="/about">About</Link>
+//                         </li>
+//                         <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-red-400 hover:text-red-300 transition duration-300">
+//                             <Link href="/projects">Projects</Link>
+//                         </li>
+//                         <li className="text-xl bg-clip-text text-transparent bg-gradient-to-r from-red-400 to-red-500 hover:text-red-300 transition duration-300">
+//                             <Link href="/contact">Contact</Link>
+//                         </li>
+//                     </ul>
+//                 </div>
+//                 <div></div>
+//             </div>
+//         </nav>
+//     );
+// };

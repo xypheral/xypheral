cd25aa472d1861037f9b115b2eb3fda8ad235080
Wei Han <noelng26@gmail.com>
2024-01-06 14:56:59 +0800

    updates

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -6,6 +6,14 @@ export default function Projects() {
       <div className="flex items-center justify-center h-screen">
         <h1 className="text-4xl text-red-400">My Projects</h1>
       </div>
+
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl text-red-400">Page 2</h1>
+      </div>
+
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-4xl text-red-400">Page 3</h1>
+      </div>
     </RootLayout>
   )
 }

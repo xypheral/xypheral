a9b7c772533cf2836f7a5f646d7e0e7667db7647
Wei Han <87291797+xypheral@users.noreply.github.com>
2024-01-12 23:14:41 +0800

    updates

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,20 +1,4 @@
-// Card based on `Discord Profile Card`
-// Change the sizing by w-[`size`px] h-[`size`px]
-
-// Original Card without Banner Image
-
-// const Card = ({ children }) => (
-//     <div className="relative bg-gradient-to-b from-black to-cyan-500 rounded-lg shadow-xl p-8 w-[400px] h-[600px] flex items-center justify-center">
-
-//       {children}
-//     </div>
-//   );
-  
-// export default Card;
-
-
-// Card with Banner Image & Desc below
-
+// Version 1.1.0 | With Banner Image & Desc
 import React, { ReactNode } from 'react';
 
 interface CardProps {
@@ -23,6 +7,7 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({ children, bannerImage }) => (
+    // Change the sizing by w-[`size`px] h-[`size`px]
     <div className="relative bg-gradient-to-b from-black to-cyan-500 rounded-lg shadow-xl p-8 w-[400px] h-[600px] flex flex-col items-center justify-center">
         <div className="absolute inset-1.5 bg-black bg-opacity-40 rounded-lg">
             <div className="absolute top-0 left-0 w-full h-1/5 rounded-t-lg" 
@@ -36,3 +21,15 @@ const Card: React.FC<CardProps> = ({ children, bannerImage }) => (
 );
 
 export default Card;
+
+
+// Version 1.0.0 | Original Card without Banner Image
+
+// const Card = ({ children }) => (
+//     <div className="relative bg-gradient-to-b from-black to-cyan-500 rounded-lg shadow-xl p-8 w-[400px] h-[600px] flex items-center justify-center">
+
+//       {children}
+//     </div>
+//   );
+  
+// export default Card;

1597e28247dc3b24e76070648d887ee13b84ced3
Wei Han <87291797+xypheral@users.noreply.github.com>
2024-01-15 22:21:11 +0800

    updates

--- src/pages/shabi.tsx
+++ src/pages/shabi.tsx
@@ -44,6 +44,21 @@ export default function Shabi() {
             </div>
 
         </div>
+
+        <div className="relative bg-black h-screen flex flex-col justify-between">
+
+            <div className="absolute left-6 top-40 bottom-6 w-1 bg-cyan-400">
+                <p className="text-sm transform -rotate-90 absolute top-[-50px] left-[-21px] whitespace-nowrap text-cyan-400">Design</p>
+            </div>
+
+            <div className="w-full h-full flex flex-col justify-center items-start">
+                <p className="text-3xl ml-16 font-extrabold text-cyan-400 translate-x-10">1. 拥有掌控自我的能力</p>
+                <p className="text-2xl ml-16 text-white translate-x-10 max-w-4xl">如果你无法长期努力，也许真的不是因为你不够自律，可能因为你自律的方式不够合理，自律是长期、持续的去保持做一件事，但合理的自律是锻炼，不合理的自律才是折磨。</p>
+            </div>
+
+        </div>
+
+
     </RootLayout>
   )
 }
